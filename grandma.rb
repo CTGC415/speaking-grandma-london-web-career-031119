652ca9f79eeaf8a7031c267805f93566b010fc58
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-01 16:37:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-else return "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  end
+  else return "HUH?! SPEAK UP, SONNY!"
 end
